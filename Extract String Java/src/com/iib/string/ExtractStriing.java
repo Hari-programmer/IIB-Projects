e80bd1ad91b4060a25af673a5d6b43937ae486f2
Hari <harishm9959@gmail.com>
2023-11-29 15:45:33 +0530

    Initial commit

---
+++ Extract String Java/src/com/iib/string/ExtractStriing.java
@@ -0,0 +1,10 @@
+package com.iib.string;
+
+public class ExtractStriing {
+	
+	public static String fun(String fname, String lname){
+		String fullname =fname+" "+lname;
+		return fullname;
+	}
+	
+}
---
+++ Java1/src/com/iib/hari/Demo.java
@@ -0,0 +1,16 @@
+package com.iib.hari;
+
+public class Demo {
+	
+	public static Long demo(Long i ,Long j){
+		if (i>=j) {
+			return i;
+		}
+		else{
+			return j;
+		}
+		}
+		
+	}
+
+
